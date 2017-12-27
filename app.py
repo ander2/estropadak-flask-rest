ba3b0eb0e36a58ca9f8aa43189f11a8178b18a23
Ander Garmendia <kelertxiki@gmail.com>
2017-12-27 23:58:52 +0100

    Refactored app to flask-restful conventions. Refactored tests to use pytest.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from flask import Flask
 from flask_restful import Api
-from .models import Estropadak, Estropada, Sailkapena, Years
+from flask_cors import CORS
+from resources.estropadak import Estropadak, Estropada, Sailkapena, Years
+
+app = Flask(__name__)
 
 def routes(app):
     api = Api(app)
@@ -7,3 +11,10 @@ def routes(app):
     api.add_resource(Sailkapena, '/sailkapena/<league_id>/<year>', '/sailkapena/<league_id>/<year>/<team>')
     api.add_resource(Estropadak, '/estropadak/<league_id>/<year>')
     api.add_resource(Estropada, '/estropada/<estropada_id>')
+
+routes(app)
+cors = CORS(app, resources={r"/estropada[k]?/*": {"origins": "*"}})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test_estropadak.py
@@ -0,0 +1,74 @@
+import os
+import app
+import json
+import logging
+import pytest
+
+class TestEstropadak():
+
+    @pytest.fixture()
+    def estropadakApp(self):
+        return app.app.test_client()
+
+    def tearDown(self):
+        pass
+        #os.close(self.db_fd)
+        #os.unlink(index.app.config['DATABASE'])
+
+    def testYears(self, estropadakApp):
+        rv = estropadakApp.get('/years')
+        years = json.loads(rv.data.decode('utf-8'))
+        assert all(year in ['act', 'arc1', 'arc2', 'euskotren'] for year in years.keys())
+
+    def testEstropadakList(self, estropadakApp):
+        rv = estropadakApp.get('/estropadak/act/2010')
+        estropadak = json.loads(rv.data.decode('utf-8'))
+        assert len(estropadak) == 20
+
+    def testEstropadakListWithoutResults(self, estropadakApp):
+        rv = estropadakApp.get('/estropadak/act/1900')
+        estropadak = json.loads(rv.data.decode('utf-8'))
+        assert len(estropadak) == 0
+
+    def testEstropadakListWithWrongLeague(self, estropadakApp):
+        rv = estropadakApp.get('/estropadak/actt/2010')
+        estropadak = json.loads(rv.data.decode('utf-8'))
+        assert len(estropadak) == 0
+
+    def testEstropada(self, estropadakApp):
+        rv = estropadakApp.get('/estropada/1c79d46b8c74ad399d54fd7ee40005e3')
+        estropada = json.loads(rv.data.decode('utf-8'))
+        assert estropada['izena'] == 'III Bandera Euskadi Basque Country'
+
+    def testEstropadaNotFound(self, estropadakApp):
+        rv = estropadakApp.get('/estropada/fuck')
+        estropada = json.loads(rv.data.decode('utf-8'))
+        assert estropada == {}
+
+    def testSailkapenaWithLoweCaseLeague(self, estropadakApp):
+        rv = estropadakApp.get('/sailkapena/act/2017')
+        sailkapena = json.loads(rv.data.decode('utf-8'))
+        assert len(sailkapena.keys()) == 12
+
+    def testSailkapenaWithUpperCaseLeague(self, estropadakApp):
+        rv = estropadakApp.get('/sailkapena/ACT/2017')
+        sailkapena = json.loads(rv.data.decode('utf-8'))
+        assert len(sailkapena.keys()) == 12
+
+    def testSailkapenaForTeam(self, estropadakApp):
+        rv = estropadakApp.get('/sailkapena/ACT/2017/Orio')
+        sailkapena = json.loads(rv.data.decode('utf-8'))
+        keys = ['wins', 'positions', 'position', 'points', 'best', 'worst', 'cumulative']
+        assert all(izenburua in keys for izenburua in sailkapena.keys()) 
+
+    def testSailkapenaForTeamThatNotExists(self, estropadakApp):
+        rv = estropadakApp.get('/sailkapena/ACT/2017/Oria')
+        assert rv.status_code == 404
+        sailkapena = json.loads(rv.data.decode('utf-8'))
+        assert sailkapena == {'error': 'Team not found'}
+
+    def testSailkapenaForTeamWithYearThatNotExists(self, estropadakApp):
+        rv = estropadakApp.get('/sailkapena/ACT/1900/Orio')
+        assert rv.status_code == 404
+        sailkapena = json.loads(rv.data.decode('utf-8'))
+        assert sailkapena == {'error': 'Stats not found'}
