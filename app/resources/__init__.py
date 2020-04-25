ca684f77feba931725fe23aada6d6cd81a9cc6e0
Ander Garmendia <kelertxiki@gmail.com>
2020-04-25 12:20:49 +0200

    Define exported modules

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,5 +1,15 @@
 from .sailkapenak import Sailkapena
 from .emaitzak import Emaitzak
+from .estropadak import Estropadak
 from .urteak import Years
+from .taldeak import Taldeak, TaldeakByLeague, Plantilla
 
-__all__ = (Emaitzak, Sailkapena, Years)
+__all__ = (
+    Emaitzak,
+    Estropadak,
+    Sailkapena,
+    Years,
+    Taldeak,
+    TaldeakByLeague,
+    Plantilla
+)
