23892ba05f8faf2885f502467b7a33641baaf32e
Ander Garmendia <kelertxiki@gmail.com>
2020-04-27 10:27:25 +0200

    Add more tests for taldeak

--- test/test_taldeak.py
+++ test/test_taldeak.py
@@ -1,7 +1,4 @@
-import os
-import datetime
 import json
-import logging
 import pytest
 from app import app
 
@@ -10,9 +7,20 @@ from app import app
 def estropadakApp():
     return app.app.test_client()
 
+
 def testTaldeak(estropadakApp):
     rv = estropadakApp.get('/taldeak?league=ACT&year=2019')
     taldeak = json.loads(rv.data.decode('utf-8'))
     assert len(taldeak) == 12
-    assert all(['name' in taldea for taldea in taldeak]) 
-    assert all(['alt_names' in taldea for taldea in taldeak]) 
+    assert all(['name' in taldea for taldea in taldeak])
+    assert all(['alt_names' in taldea for taldea in taldeak])
+
+
+@pytest.mark.parametrize('year, league',
+                         [('aaa', 'ACT'),
+                          ('', 'AT'),
+                          (2015, 1),
+                          (2015, 'ATC')])
+def testTaldeakInvalidParams(estropadakApp, year, league):
+    rv = estropadakApp.get('/taldeak?league=ACT&year=aaa')
+    assert rv.status_code == 400
