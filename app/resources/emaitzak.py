4f838930946c8f6907bf5c00a28a571160a021d8
Ander Garmendia <kelertxiki@gmail.com>
2021-01-09 16:59:47 +0100

    Fix validation

--- app/resources/emaitzak.py
+++ app/resources/emaitzak.py
@@ -1,3 +1,4 @@
+import app.config
 import logging
 
 from app.db_connection import db
@@ -27,36 +28,38 @@ class EmaitzakDAO:
         league = league.upper()
         if league.lower() == 'euskotren':
             league = league.lower()
-        yearz = "{}".format(year)
-        fyearz = "{}z".format(year)
-        start = [league, yearz]
-        end = [league, fyearz]
-        try:
-            estropadak = db.get_view_result("estropadak", "all",
-                                 startkey=start,
-                                 endkey=end,
-                                 include_docs=True,
-                                 reduce=False)
-            result = []
-            alt_names = []
-            if team:
-                teams = TaldeakDAO.get_taldeak(league, year)
-                team_names = [t for t in teams if t['name'] == team]
-                if len(team_names) > 0:
-                    alt_names = team_names[0]['alt_names']
-            for estropada in estropadak:
-                estropada = estropadak_transform(estropada)
-                team_estropada = estropada
-                if len(alt_names) > 0 and hasattr(estropada, 'sailkapena'):
-                    team_estropada.sailkapena = [
-                        talde_emaitza for talde_emaitza in estropada.sailkapena
-                        if talde_emaitza.talde_izena in alt_names
-                    ]
-                if hasattr(estropada, 'sailkapena'):
-                    result.append(team_estropada)
-            return result
-        except Exception:
-            return {'error': 'Estropadak not found'}, 404
+        if year:
+            yearz = "{}".format(year)
+            fyearz = "{}z".format(year)
+            start = [league, yearz]
+            end = [league, fyearz]
+        else:
+            start = [league]
+            end = [league + 'z']
+
+        estropadak = db.get_view_result("estropadak", "all",
+                                startkey=start,
+                                endkey=end,
+                                include_docs=True,
+                                reduce=False)
+        result = []
+        alt_names = []
+        if team:
+            teams = TaldeakDAO.get_taldeak(league, year)
+            team_names = [t for t in teams if t['name'] == team]
+            if len(team_names) > 0:
+                alt_names = team_names[0]['alt_names']
+        for estropada in estropadak:
+            estropada = estropadak_transform(estropada)
+            team_estropada = estropada
+            if len(alt_names) > 0 and hasattr(estropada, 'sailkapena'):
+                team_estropada.sailkapena = [
+                    talde_emaitza for talde_emaitza in estropada.sailkapena
+                    if talde_emaitza.talde_izena in alt_names
+                ]
+            if hasattr(estropada, 'sailkapena'):
+                result.append(team_estropada)
+        return result
 
     @staticmethod
     def get_estropadak_by_team(team, league_id):
@@ -85,13 +88,16 @@ class EmaitzakDAO:
 class Emaitzak(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('league', type=str)
-        parser.add_argument('year', type=str)
-        parser.add_argument('team', type=str)
+        parser.add_argument('league', type=str, choices=app.config.LEAGUES, case_sensitive=False, help="League abbreviation", required=True)
+        parser.add_argument('year', type=int, help="Year")
+        parser.add_argument('team', type=str, help="Team name")
         args = parser.parse_args()
-        estropadak = EmaitzakDAO.get_estropadak_by_league_year(
-            args['league'],
-            args['year'],
-            args['team'])
-        return [estropada.format_for_json(estropada)
-                for estropada in estropadak]
+        try:
+            estropadak = EmaitzakDAO.get_estropadak_by_league_year(
+                args['league'],
+                args['year'],
+                args['team'])
+            return [estropada.format_for_json(estropada)
+                    for estropada in estropadak]
+        except Exception:
+            return {'error': 'Estropadak not found'}, 404
