a8a03e33811f2d6d5df28cc39c710c37b6c4cf11
Ander Garmendia <kelertxiki@gmail.com>
2022-01-22 12:22:48 +0100

    Bump version

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ app.config['SECRET_KEY'] = JWT_SECRET_KEY
 app.config['PROPAGATE_EXCEPTIONS'] = True
 jwt = JWT(app, authenticate, identity)
 
-api = Api(app, version='0.3.3', title='Estropadak API',
+api = Api(app, version='0.3.6', title='Estropadak API',
           description='Estropadak API')
 
 
